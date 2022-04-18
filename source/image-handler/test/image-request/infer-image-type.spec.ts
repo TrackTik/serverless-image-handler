5e9bcbba9d142965744aba6f807fe28d629bfd7b
Ernesto Rodriguez Ortiz <ernesto.ortiz@tracktik.com>
2022-04-18 13:24:11 -0400

    fix: return image if content-type cannot be infer
    This is very particular to TracktikCloud S3 buckets objects. We have
    uploaded a bunch of them with a generic content type
    application/octet-stream and some valid images don't pass the validation
    to infer the content type using the file header. Let's return by default
    image as the content type instead of returning a 500 error until we
    apply the fix to our objects.

--- source/image-handler/test/image-request/infer-image-type.spec.ts
+++ source/image-handler/test/image-request/infer-image-type.spec.ts
@@ -31,21 +31,13 @@ describe("inferImageType", () => {
     expect(result).toEqual("image/jpeg");
   });
 
-  it("Should pass throw an exception", () => {
+  it('Should pass if it returns "image" for an un-know number', () => {
     // Arrange
     const imageBuffer = Buffer.from([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
 
-    try {
-      // Act
-      const imageRequest = new ImageRequest(s3Client, secretProvider);
-      imageRequest.inferImageType(imageBuffer);
-    } catch (error) {
-      // Assert
-      expect(error.status).toEqual(500);
-      expect(error.code).toEqual("RequestTypeError");
-      expect(error.message).toEqual(
-        "The file does not have an extension and the file type could not be inferred. Please ensure that your original image is of a supported file type (jpg, png, tiff, webp, svg). Refer to the documentation for additional guidance on forming image requests."
-      );
-    }
+    // Act
+    const imageRequest = new ImageRequest(s3Client, secretProvider);
+    const result = imageRequest.inferImageType(imageBuffer);
+    expect(result).toEqual("image");
   });
 });
